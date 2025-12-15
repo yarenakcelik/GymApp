859bcaaa20e7922e5bad48039f3d349e9d4baff5
yarenakcelik <yarenakcelik@icloud.com>
2025-12-15 21:57:27 +0300

    Login sayfası tasarımı güzelleştirildi.

---
+++ GymApp/Models/ViewModels/AICenterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GymApp.Services;
+
+namespace GymApp.Models.ViewModels
+{
+    public class AICenterViewModel
+    {
+        public AiService.AiRequestDto Request { get; set; } = new AiService.AiRequestDto();
+
+        // Çıktı ve hata
+        public string? Result { get; set; }
+        public string? Error { get; set; }
+    }
+}
